39d5ccd7f811027c6c0321eae6079ce77e2e596e
maribelcuales <maribelcuales@gmail.com>
2020-07-08 16:43:11 +0800

    Added dark mode class to the body and used useEffect hook to toggle dark mode on and off.

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -3,6 +3,16 @@ import useLocalStorage from "./useLocalStorage";
 
 export const useDarkMode = key => {
   const [mode, setMode] = useLocalStorage("dark-mode");
+
+  const appBody = document.querySelectorAll("body");
+
+  useEffect(() => {
+    if (mode) {
+      appBody.classList.add("dark-mode");
+    } else {
+      appBody.classList.remove("dark-mode")
+    }
+  }, [mode]);
   
   return [mode, setMode ];
 }
