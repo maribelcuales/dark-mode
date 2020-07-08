ae9a940e8715d90914ac8ce8183e4428914c68dc
maribelcuales <maribelcuales@gmail.com>
2020-07-08 15:19:09 +0800

    Build function LocalStorage as a custom hook to set dark mode style to the App.

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,6 +1,12 @@
 import { useState } from "react";
 
-export const useLocalStorage = key => {
+export const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    // Get from local storage by key
+    const item = window.localStorage.getItem(key);
+    // Parse and return stored json or, if undefined, return initialValue
+    return item ? JSON.parse(item) : initialValue; 
+  });
 
-  return;
+  return [storedValue];
 }
